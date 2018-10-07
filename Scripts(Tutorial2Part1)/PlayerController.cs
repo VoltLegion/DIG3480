f8f3071c9a72e4eadada78ec3caa55fa44e1419d
VoltLegion <42982245+VoltLegion@users.noreply.github.com>
2018-10-07 13:39:10 -0400

    Add files via upload

---
+++ Scripts(Tutorial2Part1)/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+
+    private Rigidbody2D rb2d;
+    private int count;
+    
+    public Text winText;
+
+   
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D> ();
+        count = 0;
+        winText.text = "";
+        SetCountText();
+        
+    }
+
+
+    void FixedUpdate()
+    {
+        float movementHorizontal = Input.GetAxis("Horizontal");
+        float movementVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(movementHorizontal, movementVertical);
+        rb2d.AddForce(movement * speed);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win";
+        }
+    }
+}
